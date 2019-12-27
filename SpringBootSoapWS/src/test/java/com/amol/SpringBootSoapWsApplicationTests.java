c7764bde52280416818b625b7a539553f8fce562
Amol <Amol@DESKTOP-5PETJ30>
2019-12-27 16:30:48 +0530

    Spring boot soap project

---
+++ SpringBootSoapWS/src/test/java/com/amol/SpringBootSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
